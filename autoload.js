eab4773c186b42d0e0bd4eb05f4566c14b4d8297
RiceCake <gak123@yeah.net>
2022-02-20 15:47:58 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -34,7 +34,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/gak123/live2d_api@latest/"
+			cdnPath: "https://cdn.jsdelivr.net/gh/gak123/live2d_api/"
 		});
 	});
 }
